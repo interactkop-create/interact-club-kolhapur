2ae7ef1e53102a1376726fc8401208e2523b3e61
interactkop-create <interact.kop@gmail.com>
2026-02-15 10:16:27 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,4 @@
+import { useState, useEffect } from "react";
 import { Maintenance } from "./pages/Maintenance";
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
